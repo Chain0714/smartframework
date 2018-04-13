e0f0cc6ab5d8449aecc921bafc865bc736ed4b94
Chain0714 <657775391@qq.com>
2018-04-13 15:54:44 +0800

    hello world!
    Signed-off-by: Chain0714 <657775391@qq.com>

---
+++ smart4j/chapter1/src/main/java/org/chain/charter1/HelloServlet.java
@@ -0,0 +1,30 @@
+package org.chain.charter1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈功能详细描述〉
+ *
+ * @author 17032651
+ * @see [相关类/方法]（可选）
+ * @since [产品/模块版本] （可选）
+ */
+@WebServlet(value = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = dateFormat.format(new Date());
+        req.setAttribute("currentTime", currentTime);
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
+    }
+}
